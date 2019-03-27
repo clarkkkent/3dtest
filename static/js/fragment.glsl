e0ad97f8986122d6c156dc061c06aebd787f0ef7
ClarKKKent <sergeleushin@gmail.com>
2019-03-27 09:59:35 +0300

    some fix and change texture

--- static/js/fragment.glsl
+++
@@ -1,8 +0,0 @@
-uniform float time;
-uniform sampler2D img;
-varying vec2 vUv;
-
-void main() {
-    vec4 texture = texture2D(img, vUv);
-    gl_FragColor = texture;
-}
--- static/js/model.js
+++ static/js/model.js
@@ -1,9 +1,6 @@
 import * as THREE from 'three';
-import fragment from './fragment.glsl';
-import vertex from './vertex.glsl';
 
 let OrbitControls = require('three-orbit-controls')(THREE);
-
 let container = document.getElementById('container');
 
 let camera, sphere, controls, scene, renderer, geometry, texture, light, material;
@@ -25,33 +22,24 @@ function init() {
     container.appendChild(renderer.domElement);
 
     geometry = new THREE.SphereGeometry( 2, 32, 32 );
-    texture = new THREE.TextureLoader().load("../images/base/texture2.jpg");
+    texture = new THREE.TextureLoader().load("../images/base/texture4.jpg");
 
-    material = new THREE.ShaderMaterial({
-        side: THREE.DoubleSide,
-        uniforms: {
-            time: {type:'f', value: 0},
-            img: {type: 't', value: texture}
-        },
-        vertexShader: vertex,
-        fragmentShader: fragment
+
+    material = new THREE.MeshBasicMaterial({
+            map: texture
     });
 
     sphere = new THREE.Mesh( geometry, material);
     scene.add(sphere);
 
-
     camera.position.set( 0, 0, 30 );
     controls = new OrbitControls(camera, renderer.domElement);
 }
 
-
-
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+
     render();
 }
 
