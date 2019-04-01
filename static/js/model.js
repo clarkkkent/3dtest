7d3af7c178fe6451816a0e84b56532d44057dc7e
ClarKKKent <sergeleushin@gmail.com>
2019-04-01 17:16:29 +0300

    some fix

--- static/js/model.js
+++ static/js/model.js
@@ -4,7 +4,7 @@ let OrbitControls = require('three-orbit-controls')(THREE);
 
 let container = document.getElementById('container');
 
-let camera, sphere, controls, scene, renderer, geometry, texture, light, material;
+let camera, sphere1, sphere2, sphere3, sphere4, sphere5, sphere6, sphere7, sphere8, sphere9, controls, scene, renderer, geometry, texture, light, material;
 
 function init() {
     scene = new THREE.Scene();
@@ -12,7 +12,7 @@ function init() {
     camera = new THREE.PerspectiveCamera(
         70,
         window.innerWidth / window.innerHeight,
-        0.1, 100
+        0.1, 1000
     );
 
     renderer = new THREE.WebGLRenderer();
@@ -33,15 +33,61 @@ function init() {
         opacity: 1
     });
 
-    sphere = new THREE.Mesh(geometry, material);
-    sphere.position.set(0, 0, 10);
-    scene.add(sphere);
+    sphere1 = new THREE.Mesh(geometry, material);
+    sphere1.position.set(0, 0, 10);
+    scene.add(sphere1);
+
+    sphere2 = new THREE.Mesh(geometry, material);
+    sphere2.position.set(4, 0, 8.5);
+    scene.add(sphere2);
+    scene.add(sphere2);
+
+    sphere3 = new THREE.Mesh(geometry, material);
+    sphere3.position.set(6.5, 0, 5);
+    scene.add(sphere3);
+    scene.add(sphere3);
+
+    sphere4 = new THREE.Mesh(geometry, material);
+    sphere4.position.set(5.5, 0, 0.9);
+    scene.add(sphere4);
+    scene.add(sphere4);
+
+    sphere5 = new THREE.Mesh(geometry, material);
+    sphere5.position.set(2.3, 0, -1.9);
+    scene.add(sphere5);
+    scene.add(sphere5);
+
+    sphere6 = new THREE.Mesh(geometry, material);
+    sphere6.position.set(-2, 0, -1.7);
+    scene.add(sphere6);
+    scene.add(sphere6);
+
+    sphere7 = new THREE.Mesh(geometry, material);
+    sphere7.position.set(-5, 0, 1);
+    scene.add(sphere7);
+    scene.add(sphere7);
+
+    sphere7 = new THREE.Mesh(geometry, material);
+    sphere7.position.set(-5, 0, 1);
+    scene.add(sphere7);
+    scene.add(sphere7);
+
+    sphere8 = new THREE.Mesh(geometry, material);
+    sphere8.position.set(-6, 0, 5);
+    scene.add(sphere8);
+    scene.add(sphere8);
+
+    sphere9 = new THREE.Mesh(geometry, material);
+    sphere9.position.set(-4, 0, 8.5);
+    scene.add(sphere9);
+    scene.add(sphere9);
+
 
     let loader = new THREE.OBJLoader();
 
     loader.load('../images/objects/Toilet.obj', (object) => {
         let bath = object;
-        scene.add(bath);
+        // scene.add(bath);
         bath.traverse(function(node) {
             console.log(node);
             if (node.material) {
@@ -70,7 +116,7 @@ function init() {
     scene.add(spotLight);
 
     let lightHelper = new THREE.SpotLightHelper(spotLight);
-    scene.add(lightHelper);
+    // scene.add(lightHelper);
     let shadowCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
     // scene.add(shadowCameraHelper);
 
